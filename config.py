94192d894e635cb30f6f917dadffa247d8274ef3
jackmar31 <jamarjack@gmail.com>
2021-11-30 11:02:22 -0500

    SQL

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 basedir= os.path.abspath(os.path.dirname(__file__))
 
@@ -6,12 +7,16 @@ basedir= os.path.abspath(os.path.dirname(__file__))
 # Allow outside files/folders to be added to the projects
 # base directory
 
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config():
     """
         Set Config variables for the flask app.
         Using Envorinment variables where avaible
         create config variables if not done already.
     """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off message for updates in sqlalchemy
